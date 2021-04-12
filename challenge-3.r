dd4c286dd1829e24d3dc8e64aadb2c7c0e25b36e
Malte Bartels <maltechristian.bartels@haw-hamburg.de>
2021-04-12 14:28:24 +0200

    Bartels challenge-3

--- challenge-3.r
+++ challenge-3.r
@@ -1,21 +1,54 @@
 library(assertthat)
 
-
+#load libraries
+library(dplyr)
+library(tidyr)
 ### Challenge III
-# * Load both the 2020 election results ('wahlergebnisse.rds') and stadtteil_profile ('stadtteil_profil.rds').
-# * Calculate each parties' election result per district (valid ballots are basis for this calculation).
-# * Hint: investigate the function `across()` for applying calculation on multiple columns at once.
-# * Calculate the ratio of people with a migration background in the total population of each district.
-# * Compare migration ratio to results of the AfD
-# * Compare the voter turnout to both other variables.
-# * Join the two data sets.
-# * Arrange by the AFD's results in descending order. 
-# * Prepare to discuss in the next session!
-# * Hint: the final table must have the following columns: stadtteil, mig_ratio, turn_out, afd.
+# Load both the 2020 election results ('wahlergebnisse.rds') and stadtteil_profile ('stadtteil_profil.rds').
+# Calculate each parties' election result per district (valid ballots are basis for this calculation).
+# Hint: investigate the function `across()` for applying calculation on multiple columns at once.
+# Calculate the ratio of people with a migration background in the total population of each district.
+# Compare migration ratio to results of the AfD
+# Compare the voter turnout to both other variables.
+# Join the two data sets.
+# Arrange by the AFD's results in descending order. 
+# Prepare to discuss in the next session!
+# Hint: the final table must have the following columns: stadtteil, mig_ratio, turn_out, afd.
 
 
-# combined <- …
+# combined <- 
 
+# load the data
+wahlergebnisse <- readRDS("wahlergebnisse.rds")
+stadtteil_profil <- readRDS("stadtteil_profil.rds")
+# in wahlergebnissen alle "NA" Werte in "0" umwandeln 
+wahlergebnisse[is.na(wahlergebnisse)] <- 0
+
+# Calculating each parties' election result per district in relation to the other parties
+wahlergebnisse_ratio <- wahlergebnisse %>% 
+  mutate(across(c(spd, cdu, die_linke, fdp, grune, af_d, freie_wahler, odp, piraten, volt_hamburg, di_b, menschliche_welt, sedat_ayhan, sldp), function(df){
+    df / gultige_stimmen
+  })
+  )
+
+# Calculating the migration ratio "mig_ratio"
+stadtteil_profil <- stadtteil_profil %>% 
+  mutate(mig_ratio = bevolkerung_mit_migrations_hintergrund / bevolkerung)
+
+# Calculating turnout and saving "afd" results
+wahlergebnisse <- wahlergebnisse %>% 
+  mutate(
+    turn_out = wahlende / wahlberechtigte_insgesamt,
+    afd = wahlergebnisse_ratio$af_d)
+
+# Combining and arraging the final data set
+combined <- stadtteil_profil %>% 
+  left_join(wahlergebnisse, by = c("stadtteil" = "bezeichnung")) %>% 
+  select(stadtteil, mig_ratio, turn_out, afd) %>% 
+  arrange(desc(afd))
+
+  
+  
 if (
   assert_that(
     has_name(combined, "stadtteil"), msg = "Spalte 'stadtteil' fehlt"
